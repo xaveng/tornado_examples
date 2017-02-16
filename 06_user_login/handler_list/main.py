42f9847a104de84b9174ada4466b4dd6ce6069d2
JB.Park <spritecodej@gmail.com>
2017-02-16 10:55:07 +0900

    add 06_user_login

---
+++ 06_user_login/handler_list/main.py
@@ -0,0 +1,8 @@
+import tornado.web
+import utils.cipher
+from .base import BaseHandler
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("main.html", nonce = utils.cipher.get_nonce())
---
+++ 06_user_login/utils/cipher.py
@@ -0,0 +1,4 @@
+
+def get_nonce():
+    import random
+    return random.randrange(1,5)
