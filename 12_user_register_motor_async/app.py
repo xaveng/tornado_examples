611e30662b46690f2c64b197416975f4785c782e
JB.Park <spritecodej@gmail.com>
2017-03-08 11:04:58 +0900

    Use a Coroutine(initial version)

--- 12_user_register_motor_async/app.py
+++ 12_user_register_motor_async/app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
 import tornado.httpserver
@@ -6,26 +5,40 @@ import tornado.ioloop
 import tornado.web
 import tornado.options
 import os.path
-import handler_list
+import motor
+import pprint
 
+from tornado import gen
 from tornado.options import define, options
 
 define("port", default=3000, help="run on the given port", type=int)
+db = motor.MotorClient('localhost').testdb
+
+@gen.coroutine
+def getdata():
+    data = yield db.userinfo.find_one()
+    return data
+
+class MainHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        data = yield db.userinfo.find_one()
+        pprint.pprint(data)
+        self.write("Hello, World%s" %data)
 
 class Application(tornado.web.Application):
     def __init__(self):
         base_dir = os.path.dirname(__file__)
-        handlers = handler_list.get_handler_list
+        handlers = [
+           tornado.web.url(r"/", MainHandler, name="main"),
+        ]
         settings = {
-            'cookie_secret' : '2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ',
-            'login_url' : '/login',
             'template_path' : os.path.join(base_dir, 'templates'),
             'static_path' : os.path.join(base_dir, 'static'),
             'debug' : True,
-            'xsrf_cookies' : False,
         }
         tornado.web.Application.__init__(self, handlers=handlers, **settings)
- 
+
 def main():
     tornado.options.parse_command_line()
     server = tornado.httpserver.HTTPServer(Application())
@@ -33,6 +46,5 @@ def main():
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-    print("Open http://127.0.0.1:{}".format(options.port))
-    main()
-
+    print("Oen http://127.0.0.1:{}".format(options.port))
+    main() 
--- 12_user_register_motor_async/handler_list/main.py
+++
@@ -1,9 +0,0 @@
-import tornado.web
-
-from utils.cipher import *
-from .base import BaseHandler
-
-class MainHandler(BaseHandler):
-    @tornado.web.authenticated
-    def get(self):
-        self.render("main.html",nonce = self.get_secure_cookie("user"))
