7613c36b2676daab2bedd889f46d565f17de4448
JB.Park <spritecodej@gmail.com>
2017-03-10 17:36:37 +0900

    ing.. template

---
+++ 02_template_1/app.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import os.path
+
+from tornado.options import define, options
+define("port", default=3000, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        base_dir = os.path.dirname(__file__)
+        settings = {
+            "template_path":os.path.join(base_dir, "templates"),
+            "static_path":os.path.join(base_dir, "static"),
+            "debug":True,
+        }
+        tornado.web.Application.__init__(self, [
+            tornado.web.url(r"/", MainHandler, name="main"),
+        ], **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def main():
+    tornado.options.parse_command_line()
+    Application().listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    print ("Open http://127.0.0.1:{}".format(options.port))
+    main()
+
