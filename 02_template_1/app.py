9760284d73258096b629ff23793e72a3e1505b5c
sprite <spritecodej@gmail.com>
2017-03-13 11:16:44 +0900

    template1, data transaction prototype

--- 02_template_1/app.py
+++ 02_template_1/app.py
@@ -8,6 +8,9 @@ import os.path
 from tornado.options import define, options
 define("port", default=3000, help="run on the given port", type=int)
 
+def add(x,y):
+    return x+y
+
 class Application(tornado.web.Application):
     def __init__(self):
         base_dir = os.path.dirname(__file__)
@@ -22,8 +25,15 @@ class Application(tornado.web.Application):
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html")
-
+        value_test = "normal_value_1"
+        list_test = ["list_value_1","list_value_2","list_value_3"]
+        dict_test = dict(a="dict_value_1", b="dict_value_2")
+        list_dict_test = [dict(number=1),dict(number=2), dict(number=3)]
+        self.render("index.html", value_test = value_test,
+                                  list_test = list_test,
+                                  dict_test = dict_test,
+                                  list_dict_test = list_dict_test,
+                                  add = add)
 def main():
     tornado.options.parse_command_line()
     Application().listen(options.port)
